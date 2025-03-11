4c16410db80aad90445be8e36373575c69f62fd3
Umar Zaib <umarzzaib2@gmail.com>
2025-03-11 19:30:22 +0500

    Pricing added on home page

--- src/landingPage/Home/Pricing.jsx
+++ src/landingPage/Home/Pricing.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 
 function Pricing() {
-    return ( 
-        <>
-            <h1>
-                Pricing
-            </h1>
-        </>
-     );
+    return (
+      <>
+        <div className='home-pricing-div'>
+          <div>
+            <h3>Unbeatable pricing</h3>
+            <p>
+              We pioneered the concept of discount broking and price
+              transparency in India. Flat fees and no hidden charges.
+            </p>
+            <div className='see-pricing-icon' >
+              <a>
+                See pricing
+                <i class="fa-solid fa-arrow-right" id="icon-stats"></i>
+              </a>
+            </div>
+          </div>
+          <div className='pricing-side-div'>
+            <img src="../../../public/media/pricing0.svg"></img>
+            <p className='pricing-imgSide-paragraph'>Free account opening</p>
+
+            <img src="../../../public/media/pricing0.svg"></img>
+            <p className='pricing-imgSide-paragraph'> Free equity delivery and direct mutual funds</p>
+            
+            <img src="https://zerodha.com/static/images/other-trades.svg"></img>
+            <p className='pricing-imgSide-paragraph'> Intraday and F&O</p>
+         
+
+          </div>
+
+        </div>
+      </>
+    );
 }
 
 export default Pricing;
